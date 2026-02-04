b965825ceb2888adb21568d7402f73ec84ad5003
Tvisha <officialtvisha@gmail.com>
2026-02-04 21:42:29 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, flag = 0;
+    printf("Enter first num:\n");
+    scanf("%c",&num1);
+    printf("Enter second number:\n");
+    scanf(" %c",&num2); 
+    // \\ Here we need to give space before %c as we used \n while entering
+    // \\ first number and then we press enter so that gets stored in second number so to avoid  
+    // \\ it we give space before %c in second.
+        
+    // }
+    
+
+    return 0;
+}
